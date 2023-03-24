42ddb23040bf4c3f6e12c8550fb5a6bb1fbc04e6
Patrick Roberts <patrob22@gmail.com>
2023-03-23 22:10:33 -0400

    lab complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
-}
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    let input = e.target.searchByID.value
+
+    fetch(`http://localhost:3000/movies/${input}`)
+    .then((resp) => {
+        return resp.json();
+    }).then((data) => {
+        const title = document.querySelector('h4');
+        const summary = document.querySelector('p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary
+    });
+  });
+};
 
 document.addEventListener('DOMContentLoaded', init);
